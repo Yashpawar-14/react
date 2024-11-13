a2b319cb156ffd3a21ad1011a5b3903137f24c13
Yash Pawar <yashp6559@gmail.com>
2024-11-13 18:24:41 +0530

    update

---
+++ project1/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Herosection from './components/herosection'
+import Navigation from './components/nav'
+
+function App() {
+  
+
+  return (
+    <>
+     <Navigation />
+     <Herosection />
+    </>
+  )
+}
+
+export default App
---
+++ project1/src/components/herosection.jsx
@@ -0,0 +1,29 @@
+const Herosection = () => {
+    return (
+        <>
+            <div className="main container">
+                <div className="herotext">
+                    <h1>YOUR FEET DESERVE THE BEST </h1>
+                    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+
+                    <div className="herobtn">
+                        <button>Shop Now</button>
+                        <button className="cat">Category</button>
+                    </div>
+                    <div className="shopping">
+                        <p>Availabel On</p>
+                        <div className="brandicons">
+                            <img src="/images/flipkart.png" alt="" />
+                            <img src="/images/amazon.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="heroimage">
+                    <img src="/images/shoe_image.png" alt="" />
+                </div>
+            </div>
+        </>
+    )
+}
+export default Herosection
